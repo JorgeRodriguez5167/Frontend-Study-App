16da630b37b24b5037158a9cdd978783c082bc7b
Sebastian Alvarez <sebalv@outlook.com>
2025-04-20 20:51:23 -0500

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -103,7 +103,7 @@ export default function HomeScreen() {
               data={mockNotes[activeCategory]}
               renderItem={renderNoteItem}
               keyExtractor={(item) => item.id}
-              scrollEnabled={True}
+              scrollEnabled={False}
               style={styles.notesList}
             />
           </View>
